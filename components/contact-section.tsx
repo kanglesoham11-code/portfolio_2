e591a1275ea4624f0061bac5f09e389f189dcb7d
Soham Kangle <kanglesoham11@gmail.com>
2026-08-17 01:22:47 +0530

    feat: replace mailto with direct Gmail compose link to bypass OS mail clients

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -16,7 +16,7 @@ export function ContactSection() {
 
       <ScrollReveal delay={200}>
         <div className="contact__links">
-          <a href="mailto:kanglesoham11@gmail.com?subject=Reaching%20Out:%20Let's%20Connect!&body=Hi%20Soham,%0D%0A%0D%0AI%20came%20across%20your%20portfolio%20and%20was%20really%20impressed%20by%20your%20work.%20I%20would%20love%20to%20connect%20and%20discuss%20a%20potential%20opportunity%20with%20you!%0D%0A%0D%0ABest%20regards," className="contact__link">
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=kanglesoham11@gmail.com&su=Reaching%20Out:%20Let's%20Connect!&body=Hi%20Soham,%0D%0A%0D%0AI%20came%20across%20your%20portfolio%20and%20was%20really%20impressed%20by%20your%20work.%20I%20would%20love%20to%20connect%20and%20discuss%20a%20potential%20opportunity%20with%20you!%0D%0A%0D%0ABest%20regards," target="_blank" rel="noreferrer" className="contact__link">
             kanglesoham11@gmail.com
           </a>
           <a
